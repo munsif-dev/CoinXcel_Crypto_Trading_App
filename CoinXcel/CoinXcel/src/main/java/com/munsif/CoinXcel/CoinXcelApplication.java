f6289e26d623d06cdbaf57d967fc4f7d5221e684
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-12-17 15:11:49 +0530

    initial

---
+++ CoinXcel/CoinXcel/src/main/java/com/munsif/CoinXcel/CoinXcelApplication.java
@@ -0,0 +1,24 @@
+package com.munsif.CoinXcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinXcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinXcelApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+//Using generated security password: 6b9138e5-f97d-4da4-859e-03545d37e868
+
+//This generated password is for development use only. Your security configuration must be updated before running your application in production.
+// 6b9138e5-f97d-4da4-859e-03545d37e868
---
+++ CoinXcel/CoinXcel/src/test/java/com/munsif/CoinXcel/CoinXcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munsif.CoinXcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinXcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
