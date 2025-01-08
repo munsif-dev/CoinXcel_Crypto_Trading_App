24c5a7883ed82fdc81fc5cf28e9365af1491666c
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2025-01-08 17:22:18 +0530

    pom.xml updated with spring security and jwts

--- CoinXcel/CoinXcel/src/main/java/com/munsif/CoinXcel/CoinXcelApplication.java
+++ CoinXcel/CoinXcel/src/main/java/com/munsif/CoinXcel/CoinXcelApplication.java
@@ -10,7 +10,14 @@ public class CoinXcelApplication {
 		SpringApplication.run(CoinXcelApplication.class, args);
 	}
 
-}
+	// In Sprintboot Security We need to give userName as "user" and password as the generated password in the console.
+	// Using generated security password: 6b9138e5-f97d-4da4-859e-03545d37e868
+	// This generated password is for development use only. Your security configuration must be updated before running your application in production.
+}	// Then for the deployment we need to update the security configuration in the application.properties file.
+	// spring.security.user.name=user
+ 	// spring.security.user.password=dd41e55d-dbee-45e6-b7f2-c7ba9c2af4f4
+
+
 
 
 // fgdg
--- CoinXcel/CoinXcel/src/main/java/com/munsif/CoinXcel/controller/AuthController.java
+++ CoinXcel/CoinXcel/src/main/java/com/munsif/CoinXcel/controller/AuthController.java
@@ -5,21 +5,27 @@ import com.munsif.CoinXcel.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
-    public ResponseEntity<User> register(User user){
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user){
         User newUser = new User();
         newUser.setEmail(user.getEmail());
         newUser.setFullname(user.getFullname());
+        newUser.setPassword(user.getPassword());
+
 
         User savedUser = userRepository.save(newUser);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+
+
+
+
     }
 }
